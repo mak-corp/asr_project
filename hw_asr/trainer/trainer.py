6fe09a18d1062ab3b27f25a239ed66fe2ef01338
mak-corp <mishanewyear@mail.ru>
2022-10-17 00:26:32 +0300

    Fixed error.

--- hw_asr/trainer/trainer.py
+++ hw_asr/trainer/trainer.py
@@ -278,7 +278,7 @@ class Trainer(BaseTrainer):
         self.writer.add_image("spectrogram", ToTensor()(image))
 
     def _log_audio(self, audio_batch):
-        audio = random.choice(audio_batch.cpu())
+        audio = random.choice(audio_batch)
         self.writer.add_audio("audio", audio, self.config["preprocessing"]["sr"])
 
     @torch.no_grad()
